97d865bd9a5bd0d9ce0bc4e2314931e8cd2197b4
Brayan Alfaro G <alfabrayan12@gmail.com>
2022-05-11 21:13:33 -0600

    try catch reporte

--- report/main.py
+++ report/main.py
@@ -3,8 +3,11 @@ import report as report
 
 
 def main():
-    timelines = parser.parse_timeline()
-    arribos, nombre_algoritmo, marcianos = parser.parse_marcianos()
-    report.plot_report(marcianos, timelines, arribos, nombre_algoritmo)
+    try:
+        timelines = parser.parse_timeline()
+        arribos, nombre_algoritmo, marcianos = parser.parse_marcianos()
+        report.plot_report(marcianos, timelines, arribos, nombre_algoritmo)
+    except:
+        print("Reporte no generado")
 
 main()
